6aa19b524fc794e5a28ba51733e8c4b927809a11
mlabouardyfox <mohamed@foxintelligence.fr>
2020-03-22 19:44:15 +0100

    fix terraform interpolation format

--- chapter5/vpc.tf
+++ chapter5/vpc.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "default" {
-  cidr_block           = "${var.cidr_block}"
+resource "aws_vpc" "management" {
+  cidr_block           = var.cidr_block
   enable_dns_hostnames = true
 
-  tags {
-    Name   = "${var.vpc_name}"
-    Author = "${var.author}"
+  tags = {
+    Name   = var.vpc_name
+    Author = var.author
   }
 }
