f341ea51aa3a3ec495a58cca26c212622e0f1524
mlabouardy <mohamed@labouardy.com>
2021-06-10 21:23:37 +0200

    fixed the installation of docker

--- chapter6/gcp/packer/worker/setup.sh
+++ chapter6/gcp/packer/worker/setup.sh
@@ -5,11 +5,10 @@ yum remove -y java
 yum install -y java-1.8.0-openjdk
 
 echo "Install Docker engine"
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum update -y
-yum install -y docker-ce-18.09.1
-systemctl start docker
+yum install docker -y
+usermod -aG docker ec2-user
+systemctl enable docker
 
 echo "Install git"
-yum install -y git jq
+yum install -y git
