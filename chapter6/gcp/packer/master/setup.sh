5a9c710d3aa7b1c2bad78d5227e4010a9ca72f8a
mlabouardy <mohamed@labouardy.com>
2021-06-10 21:30:22 +0200

    fixed the master setup

--- chapter6/gcp/packer/master/setup.sh
+++ chapter6/gcp/packer/master/setup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-echo "Install utils tools"
-yum install -y wget unzip
-
 echo "Install Jenkins stable release"
 yum remove -y java
 yum install -y java-1.8.0-openjdk
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins
 chkconfig jenkins on
 
@@ -21,16 +18,12 @@ chown -R jenkins:jenkins /var/lib/jenkins/.ssh
 chmod 700 /var/lib/jenkins/.ssh
 mv /tmp/id_rsa /var/lib/jenkins/.ssh/id_rsa
 chmod 600 /var/lib/jenkins/.ssh/id_rsa
+chown -R jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
 
 echo "Configure Jenkins"
 mkdir -p /var/lib/jenkins/init.groovy.d
-mv /tmp/scripts/basic-security.groovy /var/lib/jenkins/init.groovy.d/basic-security.groovy
-mv /tmp/scripts/disable-cli.groovy /var/lib/jenkins/init.groovy.d/disable-cli.groovy
-mv /tmp/scripts/csrf-protection.groovy /var/lib/jenkins/init.groovy.d/csrf-protection.groovy
-mv /tmp/scripts/disable-jnlp.groovy /var/lib/jenkins/init.groovy.d/disable-jnlp.groovy
-mv /tmp/scripts/node-agent.groovy /var/lib/jenkins/init.groovy.d/node-agent.groovy
-mv /tmp/scripts/skip-jenkins-setup.groovy /var/lib/jenkins/init.groovy.d/skip-jenkins-setup.groovy
-mv /tmp/config/jenkins /etc/sysconfig/jenkins
-chmod +x /tmp/config/install-plugins.sh
-bash /tmp/config/install-plugins.sh
+mv /tmp/*.groovy /var/lib/jenkins/init.groovy.d/
+mv /tmp/jenkins /etc/sysconfig/jenkins
+chmod +x /tmp/install-plugins.sh
+bash /tmp/install-plugins.sh
 service jenkins start
