2e6673a279b7a62c54a3266e31845c758c95b9a2
mlabouardy <mohamed@labouardy.com>
2021-06-20 20:16:47 +0200

    upgrade telergaf v1.19.0

--- chapter13/telegraf/setup.sh
+++ chapter13/telegraf/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://dl.influxdata.com/telegraf/releases/telegraf-1.14.3-1.x86_64.rpm
-yum localinstall -y telegraf-1.14.3-1.x86_64.rpm
+wget https://dl.influxdata.com/telegraf/releases/telegraf-1.19.0-1.x86_64.rpm
+yum localinstall -y telegraf-1.19.0-1.x86_64.rpm
 systemctl enable telegraf
 systemctl restart telegraf
