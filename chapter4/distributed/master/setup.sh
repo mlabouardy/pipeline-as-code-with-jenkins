b649399d5679dfaa3a366a5a97b292f01e517923
Mohamed Labouardy <mohamed@labouardy.com>
2023-05-28 17:01:55 +0200

    fix: jenkins installation #30

--- chapter4/distributed/master/setup.sh
+++ chapter4/distributed/master/setup.sh
@@ -5,9 +5,9 @@ amazon-linux-extras install epel -y
 
 echo "Install Jenkins stable release"
 yum remove -y java
-yum install -y java-1.8.0-openjdk
-wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+amazon-linux-extras install java-openjdk11 -y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install -y jenkins
 chkconfig jenkins on
 
